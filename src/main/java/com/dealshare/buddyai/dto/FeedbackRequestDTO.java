549931d1881bf1defb1b9770878c9b22957a36c6
CHAITHANYA <chaithanyakrishna3998@gmail.com>
2026-01-01 22:38:10 +0530

    dev for customer support ticket creation, storage

--- src/main/java/com/dealshare/buddyai/dto/FeedbackRequestDTO.java
+++ src/main/java/com/dealshare/buddyai/dto/FeedbackRequestDTO.java
@@ -18,6 +18,14 @@ public class FeedbackRequestDTO {
     private String comments;
     private String channel;
     private Boolean allow_contact;
+    
+    // Manual getters for snake_case fields (Lombok workaround for Java 25)
+    public Integer getUser_id() { return user_id; }
+    public void setUser_id(Integer user_id) { this.user_id = user_id; }
+    public Integer getOrder_id() { return order_id; }
+    public void setOrder_id(Integer order_id) { this.order_id = order_id; }
+    public Boolean getAllow_contact() { return allow_contact; }
+    public void setAllow_contact(Boolean allow_contact) { this.allow_contact = allow_contact; }
 }
 
 
